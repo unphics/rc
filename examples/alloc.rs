cf918ff7f28a6cd119c82060d8858289b8731717
tencent_cloud_zys <zys15664510728@gmail.com>
2024-09-30 15:20:22 +0800

    free的释放内存有效

--- examples/alloc.rs
+++ examples/alloc.rs
@@ -1,6 +1,7 @@
 use rc::malloc;
 use rc::free;
 use rc::deref;
+use std::time::Duration;
 
 struct A {
     id: u32,
@@ -32,7 +33,19 @@ impl Drop for A {
     }
 }
 
+struct B {
+    arr: [u64; 10000]
+}
+impl B {
+    pub fn new() -> Self {
+        return B{arr:[123; 10000]}
+    }
+}
+
 fn main() {
+    let a = malloc(A::new());
+    deref(a).add();
+    free(a);
     for _ in 0..1000 {
         let mut a = A::new();
         a.add();
@@ -48,4 +61,17 @@ fn main() {
     unsafe {
         println!("COUNT = {}", COUNT);
     }
+    println!("001");
+    let mut vec = Vec::new();
+    for _ in 0..1000 {
+        let p = malloc(B::new());
+        vec.push(p);
+    }
+    std::thread::sleep(Duration::from_secs(5));
+    println!("002");
+    for p in vec {
+        free(p);
+    }
+    std::thread::sleep(Duration::from_secs(5));
+    println!("003");
 }
