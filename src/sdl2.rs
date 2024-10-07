dc8eb3a8e96cf099dbdac03f7e3887fde54a836f
unphics <481381521@qq.com>
2024-10-07 10:58:40 +0800

    包含sdl2

--- src/sdl2.rs
+++ src/sdl2.rs
@@ -1,4 +1,19 @@
-mod sdl2_bindings {
-    include!(concat!(env!("OUT_DIR"), "/sdl2_bindings.rs"));
-}
+mod sdl2_bindings {include!(concat!(env!("OUT_DIR"), "/sdl2_bindings.rs"));}
 pub use sdl2_bindings::*;
+use std::ffi::CString;
+
+pub const SDL_INIT_VIDEO: u32 = 32;
+
+pub fn init(flags: u32) -> ::std::os::raw::c_int {
+    unsafe {return SDL_Init(flags);}
+}
+
+pub fn get_err() -> String {
+    let err: String;
+    unsafe {
+        let error = SDL_GetError();
+        let c_str = CString::from_raw(error as *mut i8);
+        err = String::from(c_str.to_str().unwrap());
+    }
+    return err;
+}
